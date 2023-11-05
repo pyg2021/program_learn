656e756a5760db3dd14c56a05b229e5b90086471
pengyaoguang <pengyaoguang2021@163.com>
2023-11-05 23:47:06 +0800

    update 3D net

--- program/shengli/3D_net.py
+++ program/shengli/3D_net.py
@@ -24,26 +24,28 @@ class net(nn.Module):
         self.in_channels=in_channels
         self.out_channels=out_channels
         self.is_batchnorm=is_batchnorm
-        self.conv3d=nn.Conv3d(in_channels, out_channels, 3, 1, 1)
-        self.batch=nn.BatchNorm3d(out_channels)
-        self.pool=nn.MaxPool3d(2,2)
-        self.conv1=conv_net(self.in_channels,self.out_channels)
-        self.conv2=conv_net(self.out_channels,self.out_channels)
+        # self.conv3d=nn.Conv3d(in_channels, out_channels, 3, 1, 1)
+        # self.batch=nn.BatchNorm3d(out_channels)
+        # self.pool=nn.MaxPool3d(2,2)
+        self.conv1=conv_net(self.in_channels,6)
+        self.conv2=conv_net(6,6)
+        self.conv3= conv_net(6,self.out_channels)
     def forward(self,input):
-        print(input.shape)
-        # x=self.conv3d(input)
-        # print(x.shape)
-        # x=self.batch(x)
-        # print(x.shape)
-        # x=self.pool(x)
-        # print(x.shape)
         x=self.conv1(input)
-        x=self.conv2(input)
+        x=self.conv2(x)
+        x=self.conv3(x)
         return x
 
-x=torch.ones(100,1, 100,150,200).to("cuda")
-model=net(1,1).to("cuda")
-# print(net())
-y=model(x)
-# print(y)
+x=torch.ones(1,1, 100,150,200).to("cuda:2")
+model=net(1,1).to("cuda:2")
+y=2*x
+epoch=100000
+optimizer = torch.optim.AdamW(model.parameters(),lr=1e-4)
+loss_1=torch.nn.L1Loss()
+for i in range(epoch):
+    optimizer.zero_grad()
+    loss=loss_1(model(x),y)
+    loss.backward()
+    optimizer.step()
+    print(i,loss)
 print(time.time()-start,"s")
