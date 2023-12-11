ffe7d6f2c65653853583116ab8296d09163e1c9f
pengyaoguang <pengyaoguang2021@163.com>
2023-12-11 15:30:06 +0800

    1313

--- program/shengli/3D_net6_4.py
+++ program/shengli/3D_net6_4.py
@@ -25,7 +25,7 @@ x_3,y_3=DataLoad(10000,10000+50)
 x=np.concatenate((x_1,x_2,x_3),axis=0)
 y=np.concatenate((y_1,y_2,y_3),axis=0)
 
-
+trian_number=y.shape[0]
 train_data=data_utils.TensorDataset(torch.from_numpy(x).float(),torch.from_numpy(y).float())
 train_loader = data_utils.DataLoader(train_data,batch_size=BatchSize,shuffle=True)
 # x=torch.from_numpy(x).float().to(device)
@@ -36,6 +36,7 @@ x_2,y_2=DataLoad(5000+71,5000+81)
 x_3,y_3=DataLoad(10000+51,10000+61)
 x=np.concatenate((x_1,x_2,x_3),axis=0)
 y=np.concatenate((y_1,y_2,y_3),axis=0)
+test_number=y.shape[0]
 test_data=data_utils.TensorDataset(torch.from_numpy(x).float(),torch.from_numpy(y).float())
 test_loader = data_utils.DataLoader(test_data,batch_size=BatchSize,shuffle=True)
 
@@ -58,7 +59,9 @@ test_loss_all=[]
 for epoch_i in range(epoch):
     epoch_loss=0
     model.train()
+    sum_1=0
     for i,(x,y) in enumerate(train_loader):
+        sum_1+=1
         x=x.to(device)
         y=y.to(device)
         optimizer.zero_grad()
@@ -68,18 +71,22 @@ for epoch_i in range(epoch):
         optimizer.step()
         scheduler.step()
         epoch_loss+=loss.detach().cpu().item()
+    epoch_loss=epoch_loss/sum_1
     loss_all.append(epoch_loss)
     
 
     test_loss=0
+    sum_2=0
     with torch.no_grad():
         model.eval()
         for j,(x,y) in enumerate(test_loader):
+            sum_2+=1
             x=x.to(device)
             y=y.to(device)
             y_1=model(x)
             loss=loss_1(y_1,y)+2*loss_1(torch.clamp(y_1,1.5,8),y_1)
             test_loss+=loss.detach().cpu().item()
+    test_loss=test_loss/sum_2
     test_loss_all.append(test_loss)
     print(' epoch: ',epoch_i," train_loss: ",epoch_loss," test_loss: ",test_loss)
     if epoch_i%50==0 and epoch_i>=40:
