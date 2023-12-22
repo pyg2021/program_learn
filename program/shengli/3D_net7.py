db138ded26cd4b82f1d64dfe14048ce3c304452d
pengyaoguang <pengyaoguang2021@163.com>
2023-12-22 09:21:28 +0800

    1313

--- program/shengli/3D_net7.py
+++ program/shengli/3D_net7.py
@@ -1,3 +1,4 @@
+#通过平层去预测断层以及褶皱层
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -11,38 +12,39 @@ from DataLoad import DataLoad
 from Model3D_unt import net
 import os 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID" 
-os.environ['CUDA_VISIBLE_DEVICES'] = "1,2,3"
+os.environ['CUDA_VISIBLE_DEVICES'] = "0,1,2,3"
 start=time.time()
 
 ##data_prepare
-BatchSize=12
+BatchSize=16
 device="cuda"
-x_1,y_1=DataLoad(0,0+10)
-x_2,y_2=DataLoad(5000,5000+10)
-x_3,y_3=DataLoad(10000,10000+10)
-# x=np.zeros((x_1.shape[0]+x_2.shape[0]+x_3.shape[0],2,100,100,100))
-# y=np.zeros((y_1.shape[0]+y_2.shape[0]+y_3.shape[0],1,100,100,100))
-x=np.concatenate((x_1,x_2,x_3),axis=0)
-y=np.concatenate((y_1,y_2,y_3),axis=0)
-
-
-# x,y=DataLoad(10000,10000+0)
-
-
+# x_1,y_1=DataLoad(0,0+99)
+# x_2,y_2=DataLoad(5000,5000+99)
+# x_3,y_3=DataLoad(10000,10000+99)
+# x=np.concatenate((x_1,x_2,x_3),axis=0)
+# y=np.concatenate((y_1,y_2,y_3),axis=0)
+x,y=DataLoad(0,0+99)
+trian_number=y.shape[0]
 train_data=data_utils.TensorDataset(torch.from_numpy(x).float(),torch.from_numpy(y).float())
 train_loader = data_utils.DataLoader(train_data,batch_size=BatchSize,shuffle=True)
 # x=torch.from_numpy(x).float().to(device)
 # y=torch.from_numpy(y).float().to(device)
 
-
+x_1,y_1=DataLoad(100,100)
+x_2,y_2=DataLoad(5000+100,5000+110)
+x_3,y_3=DataLoad(10000+100,10000+110)
+x=np.concatenate((x_1,x_2,x_3),axis=0)
+y=np.concatenate((y_1,y_2,y_3),axis=0)
+test_number=y.shape[0]
+test_data=data_utils.TensorDataset(torch.from_numpy(x).float(),torch.from_numpy(y).float())
+test_loader = data_utils.DataLoader(test_data,batch_size=BatchSize,shuffle=True)
 
 model=net(2,1,True,True).to(device)
 model=nn.parallel.DataParallel(model)
-# model.load_state_dict(torch.load("/home/pengyaoguang/data/3D_net_model/modeltest4_2.pkl"))
 
-epoch=20000
-optimizer = torch.optim.AdamW(model.parameters(),lr=1e-3)
-scheduler=torch.optim.lr_scheduler.StepLR(optimizer,step_size=500,gamma=0.8)
+epoch=10000
+optimizer = torch.optim.AdamW(model.parameters(),lr=2e-3)
+scheduler=torch.optim.lr_scheduler.StepLR(optimizer,step_size=600,gamma=0.8)
 loss_1=torch.nn.L1Loss()
 # plt.figure()
 # plt.imshow(y.cpu().detach()[0,0,50,:,:].T)
@@ -51,30 +53,47 @@ loss_1=torch.nn.L1Loss()
 # plt.close()
 
 loss_all=[]
-
+test_loss_all=[]
 for epoch_i in range(epoch):
     epoch_loss=0
+    model.train()
+    sum_1=0
     for i,(x,y) in enumerate(train_loader):
+        sum_1+=1
         x=x.to(device)
         y=y.to(device)
         optimizer.zero_grad()
         y_1=model(x)
         loss=loss_1(y_1,y)+2*loss_1(torch.clamp(y_1,1.5,8),y_1)
-        # loss=loss_1(y_1,y)+1*loss_1(torch.clamp(y_1,1.5,8),y_1)+loss_1(y_1[:,:,50,50,:],y[:,:,50,50,:])+loss_1(y_1[:,:,10,50,:],y[:,:,10,50,:])+loss_1(y_1[:,:,50,10,:],y[:,:,50,10,:])
         loss.backward()
         optimizer.step()
         scheduler.step()
         epoch_loss+=loss.detach().cpu().item()
+    epoch_loss=epoch_loss/sum_1
     loss_all.append(epoch_loss)
-    print(epoch_i,epoch_loss)
-    if epoch_i%50==0 and epoch_i>=40:
-        print(time.time()-start,"s")
+    
+
+    test_loss=0
+    sum_2=0
+    with torch.no_grad():
+        model.eval()
+        for j,(x,y) in enumerate(test_loader):
+            sum_2+=1
+            x=x.to(device)
+            y=y.to(device)
+            y_1=model(x)
+            loss=loss_1(y_1,y)+2*loss_1(torch.clamp(y_1,1.5,8),y_1)
+            test_loss+=loss.detach().cpu().item()
+    test_loss=test_loss/sum_2
+    test_loss_all.append(test_loss)
+    print(' epoch: ',epoch_i," train_loss: ",epoch_loss," test_loss: ",test_loss)
+    if epoch_i%50==0 and epoch_i>=20:
+        print((time.time()-start)/3600,"h")
         plt.figure()
         plt.imshow(model(x).cpu().detach()[0,0,50,:,:].T)
         plt.colorbar()
         plt.savefig("/home/pengyaoguang/data/3D_net_result/v_updata7.png")
         plt.close()
-        sio.savemat("/home/pengyaoguang/data/3D_net_result/v_updata7.mat",{"v":model(x).cpu().detach()[0,0]})
         torch.save(model.state_dict(),"/home/pengyaoguang/data/3D_net_model/modeltest7.pkl")
 
         plt.figure()
@@ -84,7 +103,8 @@ for epoch_i in range(epoch):
         plt.close()
 
         plt.figure()
-        plt.plot(range(len(loss_all)-10),loss_all[10:],label="train")
+        plt.plot(range(len(loss_all)-40),loss_all[40:],label="train")
+        plt.plot(range(len(test_loss_all)-40),test_loss_all[40:],label="test")
         plt.xlabel("epoch")
         plt.ylabel("loss")
         plt.legend()
