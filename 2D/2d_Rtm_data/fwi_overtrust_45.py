523f23f24f2efa2b85d3fe0c6f80f0a9686840bc
pengyaoguang <pengyaoguang2021@163.com>
2025-02-15 21:46:03 +0800

    1313

--- 2D/2d_Rtm_data/fwi_overtrust_45.py
+++ 2D/2d_Rtm_data/fwi_overtrust_45.py
@@ -5,12 +5,12 @@ import deepwave
 from deepwave import scalar
 import numpy as np
 import scipy.io as sio
-device = torch.device('cuda:1' if torch.cuda.is_available()
+device = torch.device('cuda:2' if torch.cuda.is_available()
                       else 'cpu')
 ny = 100
 nx = 100
 dx = 4.0
-k=1
+k=0
 v_true = torch.tensor(sio.loadmat("/home/pengyaoguang/data/3D_RTM2/v{}".format(k))["v"][50]*1000).float()
 
 # Select portion of model for inversion
@@ -19,7 +19,7 @@ v_true = torch.tensor(sio.loadmat("/home/pengyaoguang/data/3D_RTM2/v{}".format(k
 # v_true = v_true[:ny, :nx]
 
 
-v_init = (torch.tensor(1/gaussian_filter(1/v_true.numpy(), 40))
+v_init = (torch.tensor(1/gaussian_filter(1/v_true.numpy(), 30))
           .to(device))
 v = v_init.clone()
 v.requires_grad_()
@@ -67,7 +67,7 @@ source_amplitudes = (
 v_true = v_true.to(device)
 plt.figure()
 plt.imshow((v_true.cpu().detach().numpy().T))
-plt.savefig("/home/pengyaoguang/program_learn/2D/2d_Rtm_data/fwi/1.png")
+plt.savefig("/home/pengyaoguang/program_learn/2D/2d_Rtm_data/fwi/4.png")
 plt.close()
 out = scalar(v_true, dx, dt, source_amplitudes=source_amplitudes,
              source_locations=source_locations,
@@ -78,9 +78,9 @@ out = scalar(v_true, dx, dt, source_amplitudes=source_amplitudes,
 receiver_amplitudes = out[-1]
 
 
-plt.figure()
-plt.imshow(receiver_amplitudes[0].detach().cpu().T,aspect='auto')
-plt.savefig("/home/pengyaoguang/program_learn/2D/2d_Rtm_data/0.png")
+# plt.figure()
+# plt.imshow(receiver_amplitudes[0].detach().cpu().T,aspect='auto')
+# plt.savefig("/home/pengyaoguang/program_learn/2D/2d_Rtm_data/0.png")
 observed_data = receiver_amplitudes
 
 
@@ -110,10 +110,11 @@ for epoch in range(n_epochs):
         torch.quantile(v.grad.detach().abs(), 0.98)
     )
     print("epoch",epoch,"loss:",loss.cpu().item())
-    plt.figure()
-    plt.imshow((v.cpu().detach().numpy().T))
-    plt.savefig("/home/pengyaoguang/program_learn/2D/2d_Rtm_data/fwi/0.png".format(epoch))
-    plt.close()
+    if epoch%100==0:
+        plt.figure()
+        plt.imshow((v.cpu().detach().numpy().T))
+        plt.savefig("/home/pengyaoguang/program_learn/2D/2d_Rtm_data/fwi/5.png".format(epoch))
+        plt.close()
 
 
 
